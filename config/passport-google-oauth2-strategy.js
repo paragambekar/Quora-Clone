e2d323c2790f0f6dabd5b207f94fed81a9b6ea1f
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-02 09:57:35 +0530

    Create passport-google-oauth2-strategy.js

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+ const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+const env = require('../config/environment');
+
+passport.use(new googleStrategy({
+
+    clientID : env.google_client_id,
+    clientSecret : env.google_client_secret,
+    callbackURL : env.google_call_back_url,
+    
+},
+    function(accessToken, refreshToken, profile, done){
+        User.findOne({ email : profile.emails[0].value }).exec(function(error,user){
+            if(error){
+                console.log("Error in google authentication",error);
+                return;
+            }
+
+            console.log(profile);
+
+            if(user){
+                return done(null, user);
+            }else{
+                User.create({
+                    name : profile.displayName,
+                    email : profile.emails[0].value,
+                    password : crypto.randomBytes(20).toString('hex'),
+                },function(error, user){
+                    if(error){
+                        console.log("Error in creating user",error);
+                        return;
+                    }
+                    return done(null,user);
+                });
+            }
+
+        });
+    }
+));
+
+
+module.exports = passport;
